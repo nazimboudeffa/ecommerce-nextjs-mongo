b861b19ca7ce2ece2c17d80efa2b19d9c1b27e37
Nazim Boudeffa <boudeffa@gmail.com>
2024-04-14 14:04:06 +0200

    Added try catch

--- src/app/api/stores/[email]/route.ts
+++ src/app/api/stores/[email]/route.ts
@@ -5,16 +5,26 @@ import { NextResponse, NextRequest } from "next/server";
 connect();
 
 export async function GET(request: NextRequest, { params } : { params: { email: string } }) {
-  const { email } = params;
+  try {
+    const { email } = params;
 
-  const foundStore = await Store.findOne({ email: email });
-  return NextResponse.json({ foundStore }, { status: 200 });
+    const foundStore = await Store.findOne({ email: email });
+    return NextResponse.json({ foundStore }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
 }
 
 export async function PUT(request: NextRequest, { params } : { params: { email: string } }) {
-  const { email } = params;
-  const { formData } = await request.json();
-
-  await Store.findOneAndUpdate({ email: email }, formData);
-  return NextResponse.json({ message: "Store updated" }, { status: 200 });
+  try {
+    const { email } = params;
+    const { formData } = await request.json();
+  
+    await Store.findOneAndUpdate({ email: email }, formData);
+    return NextResponse.json({ message: "Store updated" }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
 }
