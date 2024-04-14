b4992805949986c9ab8870386f4ca80b38660d0f
Nazim Boudeffa <boudeffa@gmail.com>
2024-04-14 16:55:26 +0200

    Some fix

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -36,10 +36,10 @@ export default function ExplorePage() {
 		<table className='text-sm text-left sm:w-7/12 w-full max-w mx-auto mt-10'>
       <thead className='text-xs text-gray-100 uppercase border-b '>
         <tr>
-          <th scope='col' className='px-1 py-3 w-0 font-medium'>
+          <th scope='col' className='px-1 py-3 font-medium'>
             Name
           </th>
-          <th scope='col' className='px-6 py-3 w-0 font-medium'>
+          <th scope='col' className='px-6 py-3 font-medium'>
             Description
           </th>
         </tr>
@@ -51,15 +51,15 @@ export default function ExplorePage() {
           <td className='px-2 py-4 font-medium whitespace-nowrap'>
             {store.name}
           </td>
-          <td className='px-6 py-4 w-full'>
+          <td className='px-6 py-4'>
             {store.description.length > 20 ? store.description.slice(0, 50) + " ..." : store.description}
           </td>
           <dialog id={`my_modal_5_${+idx}`} className="modal modal-bottom sm:modal-middle">
             <div className="modal-box">
               <h3 className="font-bold text-lg">{store.name}</h3>
               <p className="py-4">{store.description}</p>
-              <p className={`px-6 py-4`}><Link href={store.website}><Globe /></Link></p>
-              <p className={"px-6 py-4"}><Link href={store.facebook}><ExternalLink /></Link></p>
+              <p className="px-6 py-4">{store.website ? <Link href={store.website}><Globe /></Link> : ""}</p>
+              <p className="px-6 py-4">{store.facebook ? <Link href={store.facebook}><ExternalLink /></Link> : ""}</p>
               <div className="modal-action">
                 <form method="dialog">
                   {/* if there is a button in form, it will close the modal */}
